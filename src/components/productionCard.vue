50ebae46ee55ab1b853abf9c2a13d0940c2b1907
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-06 20:46:44 +0200

    flags added

--- src/components/productionCard.vue
+++ src/components/productionCard.vue
@@ -3,15 +3,33 @@ export default {
     props: {
         item: Object
     },
-
-}
+    computed: {
+        title() {
+            return this.item.title || this.item.name
+        },
+        originalTitle() {
+            return this.item.original_title || this.item.original_name
+        },
+        hasFlag() {
+            const availableFlags = ['it', 'en'];
+            return availableFlags.includes(this.item.original_language);
+        },
+        flagSrc() {
+            const url = new URL(`../assets/img/${this.item.original_language}.png`, import.meta.url);
+            return url.href
+        }
+    }
+};
 </script>
 
 <template>
     <ul>
-        <li>{{ item.title || item.name }}</li>
-        <li>{{ item.original_title || item.original_name }}</li>
-        <li>{{ item.original_language }}</li>
+        <li>{{ title }}</li>
+        <li>{{ originalTitle }}</li>
+        <li>
+            <img v-if="hasFlag" :src="flagSrc" :alt="item.original_language">
+            <span v-else>{{ item.original_language }}</span>
+        </li>
         <li>{{ item.vote_average }}</li>
     </ul>
 </template>
