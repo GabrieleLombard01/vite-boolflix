b16271b13d89406633e65235840dd7478cc52b9c
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-06 16:35:53 +0200

    searchMovie add

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script>
 import { api } from './data';
-import store from './data/store';
+import { store } from './data/store';
 import SearchForm from './components/SearchForm.vue';
 export default {
-    components: { SearchForm }
+    components: { SearchForm },
+    data() {
+        return {
+            titleFilter: ''
+        }
+    },
+    methods: {
+        setTitleFilter(term) {
+            this.titleFilter = term;
+        },
+        searchMovie() {
+            console.log(this.titleFilter)
+        }
+    }
 };
 </script>
 
 <template>
-    <SearchForm />
+    <SearchForm @term-change="setTitleFilter" @form-submit="searchMovie" />
 </template>
 
 <style></style>
