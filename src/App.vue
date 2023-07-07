3bdcee97bdd39120e31ddf082b953f62e8f8325a
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-07-07 13:54:23 +0200

    header fixed

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,27 @@ import { api } from './data';
 import { store } from './data/store';
 import SearchForm from './components/SearchForm.vue';
 import productionCard from './components/productionCard.vue';
+
 export default {
     components: { SearchForm, productionCard },
     data() {
         return {
             store,
-            titleFilter: ''
-        }
+            titleFilter: '',
+            showMovieSection: false,
+            showSeriesSection: false
+        };
     },
     computed: {
         axiosConfig() {
             const { key, language } = api;
-
             return {
                 params: {
                     language,
                     api_key: key,
                     query: this.titleFilter
                 }
-            }
+            };
         }
     },
     methods: {
@@ -34,16 +36,20 @@ export default {
                 store.movies = [];
                 store.series = [];
                 return;
-            };
+            }
 
-            this.fetchApi("search/movie", "movies");
-            this.fetchApi("search/tv", "series");
+            this.fetchApi('search/movie', 'movies', 'showMovieSection');
+            this.fetchApi('search/tv', 'series', 'showSeriesSection');
         },
+        fetchApi(endpoint, target, section) {
+            if (this[section]) {
+                return; // Do nothing if the section is already visible
+            }
 
-        fetchApi(endpoint, target) {
             axios.get(`${api.baseUri}/${endpoint}`, this.axiosConfig)
                 .then(res => {
                     store[target] = res.data.results;
+                    this[section] = true;
                 });
         }
     }
@@ -51,24 +57,27 @@ export default {
 </script>
 
 <template>
-    <!--HEADER-->
-    <header class="d-flex align-items-center justify-content-between p-2 bg-black">
-        <h1 class="col-6 text-danger">BOOLFLIX</h1>
-        <SearchForm class="col-6 text-end" @term-change="setTitleFilter" @form-submit="searchProductions" />
-    </header>
+    <div class="container-xl bg-black text-white">
+        <!-- HEADER -->
+        <header class="d-flex align-items-center justify-content-between p-2 bg-black">
+            <h1 class="col-6 text-danger">BOOLFLIX</h1>
+            <SearchForm class="col-6 text-end" @term-change="setTitleFilter" @form-submit="searchProductions" />
+        </header>
 
-    <section id="movies">
-        <h2>Movies</h2>
-        <productionCard v-for="movie in store.movies" :key="movie.id" :item="movie" />
-    </section>
+        <section id="movies">
+            <h2 :class="{ 'd-block': showMovieSection, 'd-none': !showMovieSection }">Film:</h2>
+            <productionCard v-for="movie in store.movies" :key="movie.id" :item="movie" />
+        </section>
 
-    <section id="series">
-        <h2>Series</h2>
-        <productionCard v-for="serie in store.series" :key="serie.id" :item="serie" />
-
-    </section>
+        <section id="series">
+            <h2 :class="{ 'd-block': showSeriesSection, 'd-none': !showSeriesSection }">Serie tv:</h2>
+            <productionCard v-for="serie in store.series" :key="serie.id" :item="serie" />
+        </section>
+    </div>
 </template>
-
+  
 <style lang="scss">
-@use '../src/assets/scss/style.scss'
+@use '../src/assets/scss/style.scss';
 </style>
+  
+  
--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -2,14 +2,23 @@
 export default {
     data() {
         return {
-            searchTerm: ''
-        }
+            searchTerm: '',
+            sectionVisibilitySet: false
+        };
     },
     props: {
         placeholder: String,
-        submitLabel: String,
+        submitLabel: String
     },
-    emits: ['term-change', 'form-submit']
+    emits: ['term-change', 'form-submit'],
+    methods: {
+        setSectionVisibility() {
+            if (!this.sectionVisibilitySet) {
+                this.$emit('form-submit');
+                this.sectionVisibilitySet = true;
+            }
+        }
+    }
 };
 </script>
 
@@ -17,7 +26,10 @@ export default {
     <form @submit.prevent="$emit('form-submit')">
         <input v-model.trim="searchTerm" type="text" :placeholder="placeholder || 'Cerca...'"
             @keyup="$emit('term-change', searchTerm)">
-        <button type="submit">{{ submitLabel || 'VAI' }}</button>
+        <button type="submit" @click="setSectionVisibility">{{ submitLabel || 'VAI' }}</button>
     </form>
 </template>
+  
+
+  
 
